e2d2566045adffec25dc9b13c60cef2e65ac24f1
VittoriaBasile <vichy.basile@gmail.com>
2023-03-13 17:55:04 +0100

    commit

--- index.js
+++ index.js
@@ -1,75 +1,73 @@
-class User{
-    constructor(firstName, lastName, age, location){
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.age = age;
-        this.location = location;
-    }
-    static compare(x,y){
-        if(x.age < y.age){
-            return y.firstName  + " è più vecchio di "+  x.firstName ;
-        }else if(x.age > y.age){
-            return x.firstName  + " è più vecchio di " + y.firstName;
-        }else{
-            return x.firstName + " e " + y.firstName + " sono coetanei";
-        }
-    }
-    
-}
-
-let user1= new User("Mario", "Rossi", 25, "Molfetta");
-let user2= new User("Beppe", "Pans", 28, "Molfetta");
-
-console.log(User.compare(user1,user2));
-
-
-class Pet {
-    constructor(petName, ownerName, species, breed) {
-      this.petName = petName;
-      this.ownerName = ownerName;
-      this.species = species;
-      this.breed = breed;
-    }
-  
-    static compare(pet1, pet2) {
-      return pet1.ownerName === pet2.ownerName;
+class User {
+  constructor(firstName, lastName, age, location) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+    this.location = location;
+  }
+  static compare(x, y) {
+    if (x.age < y.age) {
+      return y.firstName + " è più vecchio di " + x.firstName;
+    } else if (x.age > y.age) {
+      return x.firstName + " è più vecchio di " + y.firstName;
+    } else {
+      return x.firstName + " e " + y.firstName + " sono coetanei";
     }
   }
-  
-const form = document.querySelector('form');
-const saveButton = form.querySelector('button');
+}
 
+let user1 = new User("Mario", "Rossi", 25, "Molfetta");
+let user2 = new User("Beppe", "Pans", 28, "Molfetta");
 
-const petList = document.createElement('ul');
-const contenuto = document.querySelector('#contenuto')
+console.log(User.compare(user1, user2));
+
+class Pet {
+  constructor(petName, ownerName, species, breed) {
+    this.petName = petName;
+    this.ownerName = ownerName;
+    this.species = species;
+    this.breed = breed;
+  }
+
+  static compare(pet1, pet2) {
+    return pet1.ownerName === pet2.ownerName;
+  }
+}
+
+const form = document.querySelector("form");
+const saveButton = form.querySelector("button");
+
+const petList = document.createElement("ul");
+const contenuto = document.querySelector("#contenuto");
 contenuto.appendChild(petList);
 
-
-saveButton.addEventListener('click', function (event) {
-
-  event.preventDefault();
-
-
+saveButton.addEventListener("click", function () {
   const petName = form.petName.value;
   const ownerName = form.ownerName.value;
   const species = form.species.value;
   const breed = form.breed.value;
 
+  let pet1 = new Pet(petName, ownerName, species, breed);
 
-  const pet = new Pet(petName, ownerName, species, breed);
+  const petListItem = document.createElement("li");
 
+  const petString =
+    "petName: " +
+    pet1.petName +
+    ", " +
+    "ownerName: " +
+    pet1.ownerName +
+    ", " +
+    "species: " +
+    pet1.species +
+    ", " +
+    "breed: " +
+    pet1.breed +
+    ";";
 
-const petListItem = document.createElement('li');
+  petListItem.innerText = petString.toUpperCase();
 
-const petString = JSON.stringify(pet).replace(/[{}"]/g, ' ');
-
-petListItem.textContent = petString.toUpperCase();
-
-
-petList.appendChild(petListItem);
-
-
+  petList.appendChild(petListItem);
 
   form.reset();
 });
-
